4d73d5c7e80305821185ef10e3d5ce71ad804334
magnitt <admin@magnitt.com>
2012-08-08 00:22:07 -0400

    [Rails App and Gems Initialization]
    + Added Rails Framework
    + Added Gems
      ~ paperclip (file upload and attachments)
      ~ shoulda (testing helper)
      ~ cucumber-rails (testing)
      ~ factor_girl_rails (testing with factories)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backmasked::Application.config.secret_token = '3d9256f023776d5c1ea0f947a048d360ed5a5b88826b946503d973fb7c168c79cd783bbe981e141e97ba8d9f4395ef33bf0266293aa328abd8ee79668652cc83'
