d86025fff92a33f0e242f652f9cfaa789f7e6dfd
magnitt <admin@magnitt.com>
2012-08-14 16:44:09 -0400

    [Hotfix] Reverse!

--- app/controllers/shell_controller.rb
+++ app/controllers/shell_controller.rb
@@ -30,7 +30,7 @@ Controller Description
       @audio = input_mp3_path = params[:mp3].tempfile.path.to_s if request.post?
       new_mp3_path = "#{Rails.root}/public/audios#{input_mp3_path}"
       FileUtils.mv @audio, "#{new_mp3_path}.mp3"
-      @sox = `sox #{new_mp3_path}.mp3 #{new_mp3_path}r.mp3 speed 2`
+      @sox = `sox #{new_mp3_path}.mp3 #{new_mp3_path}r.mp3 reverse`
       render 'shell/player'
     else
       render 'shell/upload'
