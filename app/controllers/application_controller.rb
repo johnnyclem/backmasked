aec87e4b938e8857f9a81badee23969dafaa7443
magnitt <admin@magnitt.com>
2012-08-14 17:02:24 -0400

    [Updates for the Player]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+=begin
   unless  Rails.application.config.consider_all_requests_local
     rescue_from Exception, :with => :render_404
   end
@@ -10,5 +11,6 @@ class ApplicationController < ActionController::Base
     def render_404
       render :template => 'error_pages/404', :layout => false, :status => :not_found
     end
+=end
 
 end
